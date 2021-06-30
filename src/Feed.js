4e2880bba989107da8207c7e4de352faf6e2ea1c
M Usama Alvi <m.usamaalvi@gmail.com>
2021-06-30 22:57:24 +0500

    new components for tweeting.

--- src/Feed.js
+++ src/Feed.js
@@ -1,16 +1,22 @@
 import react from 'react';
 import './Feed.css'
+import TweetBox from './TweetBox.jsx'
 
 
 const Feed = () => {
 
     return (
-        // Header
+
         <div className="feed">
+            {/* // Header */}
             <div className="feed__header">
                 <h1>Home</h1>
             </div>
+            {/* tweet box */}
+            <TweetBox />
 
+
+            {/* posts */}
         </div>
 
 
---
+++ src/TweetBox.jsx
@@ -0,0 +1,11 @@
+import './TweetBox.css'
+
+const TweetBox = () => {
+    return(
+        <div className="tweetBox">
+            <h1>tweetbpx</h1>
+        </div>
+    )
+}
+
+export default TweetBox;
