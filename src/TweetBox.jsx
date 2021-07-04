b0bfe5f2c34a829697b5f96a7d6f86f97a54f132
M Usama Alvi <m.usamaalvi@gmail.com>
2021-07-04 15:36:58 +0500

    done with tweet box

--- src/TweetBox.jsx
+++ src/TweetBox.jsx
@@ -1,9 +1,27 @@
 import './TweetBox.css'
+import { Button, Avatar } from '@material-ui/core'
 
 const TweetBox = () => {
-    return(
+    return (
         <div className="tweetBox">
-            <h1>tweetbpx</h1>
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+                    <input
+                        placeholder="What's happening?"
+                        type="text"
+                    />
+                    
+                </div>
+                <input 
+                        className="tweetBox__imageInput"
+                        placeholder="Optional: Enter Image URL"
+                        type="text"
+                    />
+                <Button className="tweetBox__tweetButton">
+                    Tweet
+                </Button>
+            </form>
         </div>
     )
 }
