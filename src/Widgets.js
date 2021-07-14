0b924a258318ffaacb876673c7ab663c889378f0
M Usama Alvi <m.usamaalvi@gmail.com>
2021-07-14 23:10:41 +0500

    embedding twitter plugins

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,24 @@
 import './Widgets.css'
-import {TwitterTimeLineEmbed, TwitterShareButton, TwitterTweetEmbed} from "react-twitter-embed"
+import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from "react-twitter-embed"
+import SearchIcon from '@material-ui/icons/Search'
 
 const Widgets = () => {
     return (
         <div className="widgets">
-            <h1>Widgets</h1>
+           <div className="widgets__input">
+               <SearchIcon className="widgets__searchIcon"/>
+               <input placeholder="Search Twitter" type="text"/>
+           </div>
+
+           <div className="widgets__widgetContainer">
+                <h2>Whats happening</h2>
+                <TwitterTweetEmbed tweetId={"1413852037129940994"} />
+
+                <TwitterTimelineEmbed 
+                sourceType="profile"
+                screenName="casey"
+                options={{height: 400}}/>
+           </div>
         </div>
 
     )
